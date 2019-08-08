4e0392e62ed96279d6b4255a201e4bb2351511b3
cheungkinwong <cheungkin.wong@gmail.com>
2019-08-08 10:32:09 +0200

    added primary key

--- create_db.sql
+++ create_db.sql
@@ -1,10 +1,11 @@
-CREATE DATABASE database_note;
-CREATE TABLE database_note.notes
-(
-     Titles VARCHAR(40) NOT NULL ,
-     Author VARCHAR(40) NOT NULL ,
-     Notes TEXT NULL DEFAULT NULL ,
-     Dates TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-     PRIMARY KEY (Titles)
-) 
+CREATE DATABASE IF NOT EXISTS `database_note`;
 
+CREATE TABLE IF NOT EXISTS `database_note`.`notes` ( 
+     `Dates` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+     `Titles` VARCHAR(40) NOT NULL ,
+     `Author` VARCHAR(40) NOT NULL ,
+     `Notes` TEXT NULL DEFAULT NULL , 
+     PRIMARY KEY (`Titles`)
+) 
+COLLATE='utf8_general_ci'
+ENGINE = InnoDB;
