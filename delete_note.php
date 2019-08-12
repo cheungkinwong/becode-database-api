cb9eb423848829bb71c9254d4aee35585a4f1a57
cheungkinwong <cheungkin.wong@gmail.com>
2019-08-12 14:29:14 +0200

    added delete_note

--- delete_note.php
+++ delete_note.php
@@ -4,7 +4,7 @@ require 'config.php';
 // sanitize
 $title = test_input($_GET['title']);
 $author = test_input($_GET['author']);
-$note = test_input($_POST['note']);
+$last_updated = test_input($_GET['last_updated']);
 
 function test_input($data) {
     $data = trim($data);
@@ -25,24 +25,23 @@ if (empty($author)) {
 } else if (!preg_match("/^([a-zA-Z' ]+)$/", $author)){
     $errors['author'] = "This author name is invalid. ";
 }
-if (empty($note)) {
-    $errors['note'] = "Note is required.";
+if (empty($last_updated)) {
+    $errors['last_updated'] = "last_updated is required.";
 }
 
 // execute
 if (count($errors)> 0){
     $errors['confirm'] = "There are still errors";
 } else {
-    $sql  = "INSERT INTO db_note.tb_note (title, author, note)
-            VALUES ( :title , :author , :note)";
+    $sql  = "DELETE FROM db_note.tb_note WHERE title = :title && author = :author && last_updated = :last_updated";
 
     $statement = $conn->prepare($sql);     
-    $statement->execute([':title'=>$title , ':author'=>$author , ':note'=>$note])  ;
+    $statement->execute([':title'=>$title , ':author'=>$author , ':last_updated'=>$last_updated])  ;
 
     if ($statement) {
-        $errors['confirm'] = "Note created successfully";
+        $errors['confirm'] = "Note deleted successfully";
     } else {
-        $errors['confirm'] = "Error creating note: " .$conn->error;
+        $errors['confirm'] = "Error deleting note: " .$conn->error;
     }
 }
 
