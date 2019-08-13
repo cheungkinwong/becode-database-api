e827aefebb842d0a745043e6874914b61fa9e43e
cheungkinwong <cheungkin.wong@gmail.com>
2019-08-13 11:24:24 +0200

    updated all php with pdo exception

--- database.php
+++ database.php
@@ -1,19 +1,22 @@
 <?php
 require 'config.php';
-
-$sql  = "CREATE DATABASE IF NOT EXISTS db_note;";  
-$sql .= "CREATE TABLE IF NOT EXISTS db_note.tb_note ( 
-        last_updated TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-        title VARCHAR(32) NOT NULL ,
-        author VARCHAR(32) NOT NULL ,
-        note TEXT NULL DEFAULT NULL , 
-        PRIMARY KEY (last_updated , title, author)
-) ";
-
-if ($conn->multi_query($sql)) {
-    echo "Database created successfully <br>";
-} else {
-    echo "Error creating database: " .$conn->error;
+$errors = [ ];
+try {
+    $sql  = "CREATE DATABASE IF NOT EXISTS db_note;";  
+    $sql .= "CREATE TABLE IF NOT EXISTS db_note.tb_note ( 
+            last_updated TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+            title VARCHAR(32) NOT NULL ,
+            author VARCHAR(32) NOT NULL ,
+            note TEXT NULL DEFAULT NULL , 
+            PRIMARY KEY (last_updated , title, author)
+    )";
+    $statement = $conn->multi_query($sql); 
+    $errors['confirm'] = "Database created successfully";
+} catch(PDOException $exception){
+    $errors['confirm'] = "Error creating database: " . $exception->getMessage();
 }
-$conn->close();
+$error_json = json_encode($errors);
+echo $error_json;
+
+$conn = null;
 ?>
--- list_note.php
+++ list_note.php
@@ -1,17 +1,15 @@
 <?php
 require 'config.php';
-
-$sql  = 'SELECT *  FROM db_note.tb_note';
-$statement = $conn->query($sql);   
-
-$posts = $statement->fetchAll(PDO::FETCH_OBJ);
-$posts_json = json_encode($posts);
-echo $posts_json;
-
-if ($statement) {
+$errors = [ ];
+try {
+    $sql  = 'SELECT *  FROM db_note.tb_note';
+    $statement = $conn->query($sql);   
+    $posts = $statement->fetchAll(PDO::FETCH_OBJ);
+    $posts_json = json_encode($posts);
+    echo $posts_json;
     $errors['confirm'] = "Notes listed successfully";
-} else {
-    $errors['confirm'] = "Error listing notes: " .$conn->error;
+} catch(PDOException $exception){
+    $errors['confirm'] = "Error listing note: " . $exception->getMessage();
 }
 // status
 $error_json = json_encode($errors);
